43c21bbae3aff3d8d568cca7b5f3694179c97d81
imexsis <57748102+imexsis@users.noreply.github.com>
2020-09-09 16:18:16 +0200

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ActivityCard from "./components/ActivityCard"
 import "./App.css";
 
 
@@ -6,6 +7,7 @@ class App extends React.Component {
   render() {
     return (
      <div>
+       <ActivityCard/>
        Hello world
      </div>
     );
---
+++ frontend/src/components/ActivityCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function ActivityCard () {
+
+    return (
+        <div>
+            <p>Hello ActivityCard</p>
+        </div>
+    )
+
+}
+
+
+export default ActivityCard;
