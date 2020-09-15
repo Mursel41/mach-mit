7f7fca38cafcf870a2c1b29c0318fe394c4f47ad
imexsis <57748102+imexsis@users.noreply.github.com>
2020-09-15 21:30:30 +0200

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,22 @@ import ActivityCard from "./components/ActivityCard";
 import "./App.css";
 import Homepage from "./components/Homepage.js";
 import Container from "@material-ui/core/Container";
+import Footer from "./components/Footer";
 
 class App extends React.Component {
   render() {
     return (
+
+      <div>
       <Container maxWidth="lg">
         <Homepage />
         <ActivityCard />
+        
       </Container>
+        <div>
+          <Footer/>
+        </div>
+      </div>
     );
   }
 }
