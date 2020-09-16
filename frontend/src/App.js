5de77dc62a93088b2a61bf46453fdf5cfff458b7
Mustafa Kurukamci <isbuan51@gmail.com>
2020-09-16 12:57:44 +0200

    fixed problem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,20 @@
-import React from "react";
-import "./App.css";
-import { Signup } from "./components/signUpForm";
-import Homepage from "./components/Homepage.js";
-import Container from "@material-ui/core/Container";
-import Footer from "./components/Footer";
+import React from 'react';
+import './App.css';
+import { Signup } from './components/signUpForm';
+import Homepage from './components/Homepage.js';
+import Container from '@material-ui/core/Container';
+import Footer from './components/Footer';
 
 class App extends React.Component {
   render() {
     return (
-
       <div>
-      <Container maxWidth="lg">
-        <Homepage />
-        <ActivityCard />
-        
-      </Container>
+        <Container maxWidth="lg">
+          <Homepage />
+          <ActivityCard />
+        </Container>
         <div>
-          <Footer/>
+          <Footer />
         </div>
       </div>
     );
