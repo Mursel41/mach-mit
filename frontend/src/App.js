c0ff3ee92ccb141e49a90d865929777c26e4fa2c
Mustafa Kurukamci <isbuan51@gmail.com>
2020-09-17 14:46:17 +0200

    cleaned boilerplate, added sass

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,14 @@
-import React from "react";
-import "./App.css";
-import { Signup } from "./components/signUpForm";
-import Homepage from "./components/JoinButton.js";
-import ActivityCard from "./components/ActivityCard";
-import Container from "@material-ui/core/Container";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import MainTextHeroImg from "./components/MainTextHeroImg";
-import SearchBar from "./components/SearchBar";
-import JoinButton from "./components/JoinButton.js";
-
+import React from 'react';
+import './App.scss';
+import { Signup } from './components/signUpForm';
+import Homepage from './components/JoinButton.js';
+import ActivityCard from './components/ActivityCard';
+import Container from '@material-ui/core/Container';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import MainTextHeroImg from './components/MainTextHeroImg';
+import SearchBar from './components/SearchBar';
+import JoinButton from './components/JoinButton.js';
 
 class App extends React.Component {
   render() {
