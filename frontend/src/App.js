385791abfddf3daf598fabfa31ddf5f3cab07ecc
Mursel Genc <murselgenc41@gmail.com>
2020-09-21 11:13:51 +0200

    CreateActivity componenet is created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import './App.scss';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Homepage from './components/Homepage';
-import { Signup } from './components/signUpForm';
-import Login from './components/LogIn';
-import NotFound from './views/NotFound';
-
+import React from "react";
+import "./App.scss";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Homepage from "./views/Homepage";
+import { Signup } from "./components/signUpForm";
+import Login from "./components/LogIn";
+import NotFound from "./views/NotFound";
+import CreateActivity from "./views/CreateActivity";
 
 class App extends React.Component {
   render() {
@@ -24,6 +24,9 @@ class App extends React.Component {
             </Route>
             <Route exact path="/login">
               <Login />
+            </Route>
+            <Route exact path="/createactivity">
+              <CreateActivity />
             </Route>
             <Route>
               <NotFound />
