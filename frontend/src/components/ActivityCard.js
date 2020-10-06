721247ee2a9994690f1ebd86af0c426f1c40a5c0
Mursel Genc <murselgenc41@gmail.com>
2020-10-06 14:19:31 +0200

    conditional error solved

--- frontend/src/components/ActivityCard.js
+++ frontend/src/components/ActivityCard.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Card, Grid, Paper } from '@material-ui/core';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import moment from 'moment';
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import { Card, Grid, Paper } from "@material-ui/core";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import moment from "moment";
+import { withRouter } from "react-router-dom";
 
 class ActivityCard extends React.Component {
   constructor(props) {
@@ -16,9 +16,10 @@ class ActivityCard extends React.Component {
   render() {
     return (
       <Grid container spacing={1} justify="center">
-        {this.props.activities.length < 1 ? (
+        {this.props.activities && this.props.activities.length < 1 ? (
           <p>You don't have any activities yet</p>
         ) : (
+          this.props.activities &&
           this.props.activities.map((card) => {
             return (
               <Box
@@ -57,11 +58,11 @@ class ActivityCard extends React.Component {
                           component="p"
                         >
                           <Box>
-                            {moment(card.startDate).format('D MMM YYYY')}
+                            {moment(card.startDate).format("D MMM YYYY")}
                           </Box>
                           <Box>
-                            {' '}
-                            {card.participants.length}/{card.numberOfAttendee}{' '}
+                            {" "}
+                            {card.participants.length}/{card.numberOfAttendee}{" "}
                             Participants
                           </Box>
                         </Typography>
@@ -73,9 +74,9 @@ class ActivityCard extends React.Component {
                           mt={2}
                         >
                           <Box>
-                            {' '}
+                            {" "}
                             <Typography variant="body2" color="Primary">
-                              {card.price === 0 ? 'Free' : '€ ' + card.price}
+                              {card.price === 0 ? "Free" : "€ " + card.price}
                             </Typography>
                           </Box>
                           <Box>
