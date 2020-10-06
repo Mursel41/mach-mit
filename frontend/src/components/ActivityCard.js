d3cb81ff978f6f5e23c45513b0833f9f29f61735
Mustafa Kurukamci <isbuan51@gmail.com>
2020-10-06 11:24:02 +0200

    worked on personal home page

--- frontend/src/components/ActivityCard.js
+++ frontend/src/components/ActivityCard.js
@@ -9,79 +9,89 @@ import moment from 'moment';
 import { withRouter } from 'react-router-dom';
 
 class ActivityCard extends React.Component {
-  render(props) {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
     return (
       <Grid container spacing={1} justify="center">
-        {this.props.activities.map((card) => {
-          return (
-            <Box
-              m={0.5}
-              key={card._id}
-              width={200}
-              mx={0.5}
-              mb={0.5}
-              onClick={() => this.props.history.push(`/events/${card._id}`)}
-            >
-              <Card>
-                <Box>
-                  <CardActionArea>
-                    <CardMedia color="secondary.main">
-                      <img
-                        src={card.image}
-                        alt="activity_image"
-                        width="100%"
-                        height={140}
-                      />
-                    </CardMedia>
-                    <CardContent>
-                      <Typography
-                        gutterBottom
-                        color="secondary"
-                        variant="subtitle3"
-                      >
-                        {card.typeOfActivity.name}
-                      </Typography>
-                      <Typography gutterBottom variant="h7" component="h2">
-                        {card.title}
-                      </Typography>
-                      <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        component="p"
-                      >
-                        <Box>{moment(card.startDate).format('D MMM YYYY')}</Box>
-                        <Box>
-                          {' '}
-                          {card.participants.length}/{card.numberOfAttendee}{' '}
-                          Participants
+        {this.props.activities.length < 1 ? (
+          <p>You don't have any activities yet</p>
+        ) : (
+          this.props.activities.map((card) => {
+            return (
+              <Box
+                m={0.5}
+                key={card._id}
+                width={200}
+                mx={0.5}
+                mb={0.5}
+                onClick={() => this.props.history.push(`/events/${card._id}`)}
+              >
+                <Card>
+                  <Box>
+                    <CardActionArea>
+                      <CardMedia color="secondary.main">
+                        <img
+                          src={card.image}
+                          alt="activity_image"
+                          width="100%"
+                          height={140}
+                        />
+                      </CardMedia>
+                      <CardContent>
+                        <Typography
+                          gutterBottom
+                          color="secondary"
+                          variant="subtitle3"
+                        >
+                          {card.typeOfActivity.name}
+                        </Typography>
+                        <Typography gutterBottom variant="h7" component="h2">
+                          {card.title}
+                        </Typography>
+                        <Typography
+                          variant="body2"
+                          color="textSecondary"
+                          component="p"
+                        >
+                          <Box>
+                            {moment(card.startDate).format('D MMM YYYY')}
+                          </Box>
+                          <Box>
+                            {' '}
+                            {card.participants.length}/{card.numberOfAttendee}{' '}
+                            Participants
+                          </Box>
+                        </Typography>
+                        <Box
+                          display="flex"
+                          flexDirection="row"
+                          justifyContent="space-between"
+                          flexWrap="wrap"
+                          mt={2}
+                        >
+                          <Box>
+                            {' '}
+                            <Typography variant="body2" color="Primary">
+                              {card.price === 0 ? 'Free' : '€ ' + card.price}
+                            </Typography>
+                          </Box>
+                          <Box>
+                            <Typography variant="body2">
+                              {card.address.city}
+                            </Typography>
+                          </Box>
                         </Box>
-                      </Typography>
-                      <Box
-                        display="flex"
-                        flexDirection="row"
-                        justifyContent="space-between"
-                        flexWrap="wrap"
-                        mt={2}
-                      >
-                        <Box>
-                          {' '}
-                          <Typography variant="body2" color="Primary">
-                            {card.price === 0 ? 'Free' : '€ ' + card.price}
-                          </Typography>
-                        </Box>
-                        <Box>
-                          <Typography variant="body2">
-                            {card.address.city}
-                          </Typography>
-                        </Box>
-                      </Box>
-                    </CardContent>
-                  </CardActionArea>
-                </Box>
-              </Card>
-            </Box>
-          );
-        })}
+                      </CardContent>
+                    </CardActionArea>
+                  </Box>
+                </Card>
+              </Box>
+            );
+          })
+        )}
       </Grid>
     );
   }
