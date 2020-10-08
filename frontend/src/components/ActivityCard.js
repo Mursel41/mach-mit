c94fd2b9a2fe5e7a1531a2472d401f161e117c67
imexsis <57748102+imexsis@users.noreply.github.com>
2020-10-08 13:23:36 +0200

    TD13-Show-Not-Fouund-Search-Message & some other changes

--- frontend/src/components/ActivityCard.js
+++ frontend/src/components/ActivityCard.js
@@ -16,9 +16,10 @@ class ActivityCard extends React.Component {
   render() {
     return (
       <Grid container spacing={1} justify="center">
-        {this.props.activities && this.props.activities.length < 1 ? (
-          <p>You don't have any activities yet</p>
-        ) : (
+        {
+        // this.props.activities && this.props.activities.length < 1 ? (
+        //   <p>You don't have any activities yet</p>
+        // ) : (
           this.props.activities &&
           this.props.activities.map((card) => {
             return (
@@ -92,7 +93,8 @@ class ActivityCard extends React.Component {
               </Box>
             );
           })
-        )}
+        // )
+        }
       </Grid>
     );
   }
--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -23,6 +23,7 @@ export default class SearchBar extends React.Component {
       //location for search options
       inputLocation:'',
       message: '',
+      firstSearch: false
     };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -60,31 +61,40 @@ export default class SearchBar extends React.Component {
       this.state.inputLocation !== ""
     ) {
       searchKey = `?address.city=${this.state.inputLocation}`;
-      this.setState({ message: `${this.state.inputLocation}` });
+      this.setState({ message: ` ${this.state.inputLocation}` });
     } else if (
       this.state.inputCategory.length !== 0 &&
       this.state.inputLocation === ""
     ) {
       searchKey = `?typeOfActivity=${this.state.inputCategory._id}`;
-      this.setState({ message: `${this.state.inputCategory.name}` });
+      this.setState({ message: ` ${this.state.inputCategory.name}` });
     } else if (
       this.state.inputCategory.length !== 0 &&
       this.state.inputLocation !== ""
     ) {
       searchKey = `?typeOfActivity=${this.state.inputCategory._id}&address.city=${this.state.inputLocation}`;
       this.setState({
-        message: `${this.state.inputCategory.name} in ${this.state.inputLocation}`,
+        message: ` ${this.state.inputCategory.name} in ${this.state.inputLocation}`,
       });
     }
 
     if (searchKey !== "") {
+
+      this.setState({ activities:[] })
+
       fetch(`http://localhost:5000/api/v1/activities${searchKey}`)
         .then((res) => res.json())
-        .then((activities) => this.setState({ activities }))
+        .then((activities) => {
+            this.setState({ activities })
+        })
+        .then()
         .catch((err) => console.log(err));
+
+       
 
       this.setState({ inputLocation: "" });
       this.setState({ inputCategory: [] });
+      this.setState({ firstSearch: true });
     }
   }
 
@@ -103,7 +113,7 @@ export default class SearchBar extends React.Component {
           style={{
             backgroundColor: "white",
             width: "100%",
-            padding: "5px",
+            padding: "10px",
           }}
         >
           <form id="Search" onSubmit={this.handleSubmit}>
@@ -179,26 +189,28 @@ export default class SearchBar extends React.Component {
           style={{
             backgroundColor: "rgba(238,250,255, 0.5)",
             width: "100%",
-            padding: "5px",
+            padding: "0px 10px",
           }}
         >
-          {this.state.activities.length > 0 && (
-            <Grid item xs={12} md={9}>
+            <Grid item xs={12}>
               <Grid item>
                 <Typography variant="h4" component="h4" gutterBottom>
-                  {`Results for ${this.state.message}`}
+                {this.state.activities.length > 0 ? 
+                  <React.Fragment>
+                        Activities for {this.state.message} 
+                      <Grid item>
+                        <Divider />
+                      </Grid>
+                  </React.Fragment> :
+                  this.state.firstSearch && `Sorry, there are no activities for ${this.state.message}`
+                  }
                 </Typography>
               </Grid>
-
-              <Grid item>
-                <Divider />
-              </Grid>
-
-              <Grid item xs={12} md={9} justify="center">
-                <ActivityCard activities={this.state.activities} />
-              </Grid>
+                  <Grid item xs={12} justify="center">
+                    <ActivityCard activities={this.state.activities} />
+                  </Grid>
+           
             </Grid>
-          )}
         </Paper>
       </Container>
     );
